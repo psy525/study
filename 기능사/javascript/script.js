15f1429376c7c7c445f5ab73a5aba9ab553535b0
psy0525 <shineeluv525@gmail.com>
2024-08-13 19:38:27 +0900

    기능사
    자료

---
+++ 기능사/javascript/script.js
@@ -0,0 +1,18 @@
+let imgs = 2;
+let now = 0;
+
+start();
+
+function start() {
+  $(".imgs>img").eq(0).siblings().css({ "margin-left": "-2000px" });
+  setInterval(function () {
+    slide();
+  }, 2000);
+}
+function slide() {
+  now = now == imgs ? 0 : now + 1;
+  $(".images>img")
+    .eq(now - 1)
+    .css({ "margin-left": "-2000px" });
+  $(".imgs>img").eq(now).css({ "margin-left": "0px" });
+}
